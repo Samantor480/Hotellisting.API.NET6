dc4406331eef066c96279268c684d2fc13dfcb2a
sam antor <samantor480@hotmail.com>
2022-06-26 23:26:09 +0800

    Added Entity Framework and Created Database

--- HotellistingApi/Program.cs
+++ HotellistingApi/Program.cs
@@ -1,6 +1,18 @@
+using HotellistingApi.Data;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+var connectionstring = builder.Configuration.GetConnectionString("HotelListingDbConnectionString");
+builder.Services.AddDbContext<HotelListingDbContext>(options =>
+{
+    options.UseSqlServer(connectionstring);
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -14,6 +26,7 @@ builder.Services.AddCors(options => {
         .AllowAnyMethod());
 });
 
+builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
